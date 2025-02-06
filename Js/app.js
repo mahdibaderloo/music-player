cbb2f0f400b172156884f26aa891bb7faefc9f17
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-02-06 12:24:37 +0330

    select dom elements

--- Js/app.js
+++ Js/app.js
@@ -1,6 +1,13 @@
-// fetch ('https://api.genius.com/web_pages/lookup?canonical_url=http://example.com')
-//     .then(res => res)
-//     .then(data => console.log(data))
-    
+// DOM Elements //
 
-
+const cover = document.getElementById('cover')
+const musicName = document.getElementById('music-name')
+const singer = document.getElementById('singer')
+const like = document.getElementById('like')
+const currentTime = document.getElementById('current-time')
+const duration = document.getElementById('duration')
+const playlist = document.getElementById('playlist')
+const back = document.getElementById('back')
+const playPause = document.getElementById('play-pause')
+const next = document.getElementById('next')
+const repeat = document.getElementById('repeat')
