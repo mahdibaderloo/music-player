453e3fb961e900326a0ce252fc5f4c120ba90e19
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-02-19 10:50:34 +0330

    add data base file

---
+++ Js/data-base.js
@@ -0,0 +1,45 @@
+const media = [
+  {
+    id: 1,
+    musicName: "Toosie Slide",
+    singer: "Drake",
+    src: "https://files.musicfeed.ir/dir/2020/8/Drake%20Toosie%20Slide%20128.mp3",
+    cover:
+      "https://i1.sndcdn.com/artworks-hZD3zLFuel7YnNSa-bObqgQ-t500x500.jpg",
+    like: false,
+  },
+  {
+    id: 2,
+    musicName: "Niagara Falls (Foot or 2)",
+    singer: "Travis Scott & 21 Savage",
+    src: "https://dl.behmelody.in/1403/04/01/Travis%20Scott%20-%20Niagara%20Falls%20(Foot%20or%202)%20(128).mp3",
+    cover: "https://i1.sndcdn.com/artworks-VVX6CvBxRbey-0-t500x500.jpg",
+    like: false,
+  },
+  {
+    id: 3,
+    musicName: "Niagara Falls (Foot or 2)",
+    singer: "Travis Scott & 21 Savage",
+    src: "https://dl.behmelody.in/1403/04/01/Travis%20Scott%20-%20Niagara%20Falls%20(Foot%20or%202)%20(128).mp3",
+    cover:
+      "https://i1.sndcdn.com/artworks-XXlQzMozz5HrqFMi-ZzywGA-t1080x1080.jpg",
+    like: false,
+  },
+  {
+    id: 4,
+    musicName: "Dead Rose",
+    singer: "Mahyar",
+    src: "https://irsv.upmusics.com/dlw/Mahyar%20-%20Dead%20Rosee%20(320).mp3",
+    cover:
+      "https://solahangs.com/wp-content/uploads/2024/12/Mahyar-Dead-Rose-img.jpg",
+    like: false,
+  },
+  {
+    id: 5,
+    musicName: "Movazebam Bash",
+    singer: "The Don",
+    src: "https://dl.tiktokmusics.ir/music/Dark%20Days%20By%2021Savage.mp3",
+    cover: "https://i1.sndcdn.com/artworks-0e2cvZrtJ8vK-0-t500x500.jpg",
+    like: false,
+  },
+];
